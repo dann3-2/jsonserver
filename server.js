168195a94652417401d11dbed9f8d125e3e36110
dann3-2 <160372781+dann3-2@users.noreply.github.com>
2024-11-23 01:22:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+// Gunakan middleware bawaan JSON Server
 server.use(middlewares);
+
+// Gunakan router dari file db.json
 server.use(router);
 
+// Jalankan server
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
